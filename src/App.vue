07d49f738ea3a6b12dc4083621282810447857f7
zch <zhch_hong@163.com>
2026-08-02 09:59:27 +0800

    vscode扩展无法识别变量

--- src/App.vue
+++ src/App.vue
@@ -17,32 +17,14 @@ export default {
     window.spineplayer = new window.spine.SpinePlayer("spine-container", {
       jsonUrl: "/spine/char020401.json",
       atlasUrl: "/spine/char020401.atlas",
-      // animation: "cut_B",
-      // animations: ["cut_A", "cut_B"],
-
-      // jsonUrl: "/live2d/雪泉-雪花之舞-立绘/char020401.json",
-      // atlasUrl: "/live2d/雪泉-雪花之舞-立绘/char020401.atlas",
       animation: "idle",
-      // animations: ["cut_A", "cut_B"],
-
-      // alpha: true,
-      // backgroundColor: "#00000000",
       premultipliedAlpha: true,
       showControls: false,
       viewport: {
-        // debugRender: true,
         padLeft: "0%",
         padRight: "0%",
         padTop: "0%",
         padBottom: "0%",
-        // x: -450,
-        // y: 50,
-        // width: 1600,
-        // height: 2000,
-        // padLeft: "10%",
-        // padRight: "10%",
-        // padTop: "10%",
-        // padBottom: "10%",
       },
     });
   },
